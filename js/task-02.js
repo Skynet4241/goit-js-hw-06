4dd8a7306d0cb33c62e4349b76dc5cd1ef493302
Skynet4241 <skynet4241@gmail.com>
2022-11-11 10:49:53 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const list = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  return liEl;
+});
+
+console.log(items);
+list.append(...items);
