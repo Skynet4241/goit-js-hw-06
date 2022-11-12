3d35211b0860a7bd9c114d92204bfa896e242fca
Skynet4241 <skynet4241@gmail.com>
2022-11-12 12:12:31 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  const data = new FormData(e.target);
+
+  let values = {};
+
+  data.forEach((value, key) => {
+    values[key] = value;
+  });
+  console.log(values);
+
+  if (email.value === '' && password.value === '') {
+    alert('Error! All fields must be filled in');
+  } else {
+    alert('You are successfully logged in ');
+  }
+  e.currentTarget.reset();
+});
