e5e13ac851b7f159d5b17f50f41b7bb672ea0f27
Skynet4241 <skynet4241@gmail.com>
2022-11-12 12:20:11 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButton = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+
+changeButton.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+});
